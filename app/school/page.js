b2a159c9e5b98ba1e6eb6e29d04aa6e293a52f89
Syed Samad <syedsamad313@gmail.com>
2026-06-07 01:58:55 +0530

    ui: update portal-login ui-design

--- app/school/page.js
+++ app/school/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { School, Mail, Lock, LogIn, Shield } from "lucide-react";
+import { School, Mail, Lock, LogIn, Shield, Link, ChevronRight } from "lucide-react";
 import Loading from "@/components/ui/Loading";
 import { fetchSchools, loginSchoolUser } from "@/lib/school/authSchool";
 import { toast } from "react-toastify";
@@ -8,6 +8,7 @@ import { useRouter } from "next/navigation";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth, db } from "@/lib/firebase";
 import { doc, getDoc } from "firebase/firestore";
+import Image from "next/image";
 
 export default function SchoolLoginPage() {
   const router = useRouter();
@@ -68,87 +69,104 @@ export default function SchoolLoginPage() {
   return (
     <>
       {loading && <Loading />}
-      <div className="min-h-[100dvh] flex items-center justify-center bg-(--bg) p-5">
-        <div className="w-full max-w-md bg-(--bg-card) rounded-2xl shadow-xl border border-(--border) p-6 sm:p-8">
-
-          <div className="mb-5 text-center">
-            <div className="mx-auto mb-4 flex h-12 w-12 items-center justify-center rounded-full border border-(--border)">
-              <Shield className="h-6 w-6 text-(--primary)" />
+      <div className="min-h-[100dvh] flex items-center justify-center bg-(--bg)">
+        <div className="flex-1/2 hidden lg:flex h-dvh bg-(--primary) shadow-md flex-col items-center justify-center">
+          <div>
+            <div className="flex w-fit items-center gap-1 bg-white rounded-full p-3">
+              <Image src="/logo.png" alt="Logo" width={35} height={35} />
+              {/* <span className="font-bold text-xl tracking-tight text-(--primary)">Appitor</span> */}
             </div>
-
-            <h1 className="text-2xl font-semibold">School Login</h1>
-            <p className="mt-1 text-sm text-muted">
-              Secure access for schools & staff
-            </p>
+            <p className="text-white/75 font-bold text-4xl mt-8">WHERE</p>
+            <p className="text-white font-bold text-6xl">INNOVATION</p>
+            <p className="text-white/75 font-bold text-4xl">MEETS EDUCATION..</p>
+            <div className="mt-10 flex flex-col gap-1 items-start">
+              <div className="divide-x-2 divide-white/50 flex ">
+                <div className="text-white/90 uppercase px-2 font-medium text-xs cursor-pointer hover:underline hover:text-white transition">About us</div>
+                <div className="text-white/90 uppercase px-2 font-medium text-xs cursor-pointer hover:underline hover:text-white transition">Privacy Policy</div>
+                <div className="text-white/90 uppercase px-2 font-medium text-xs cursor-pointer hover:underline hover:text-white transition">Terms & Conditions</div>
+                <div className="text-white/90 uppercase px-2 font-medium text-xs cursor-pointer hover:underline hover:text-white transition">Help & Support</div>
+              </div>
+              <p className="text-white px-2 text-xs text-center font-medium mt-2">Copyright &copy; 2026 Appitor. All Rights Reserved.</p>
+            </div>
           </div>
-          <form onSubmit={handleLogin} className="space-y-4">
-            <div>
-              <label className="text-sm text-(--text-muted)">School</label>
-              <div className="relative">
-                <School className="absolute left-3 top-3 h-4 w-4 text-(--text-muted)" />
-                <select
-                  required
-                  value={schoolId}
-                  onChange={(e) => {
-                    setSchoolId(e.target.value);
-                    setSchoolCode(e.target.value != '' ? e.target.options[e.target.selectedIndex].text.split(' (')[1].replace(')', '') : '')
-                  }}
-                  className="w-full pl-10 pr-3 py-2 rounded-lg border border-(--border) focus:outline-none focus:ring-2 focus:ring-[var(--primary)]"
-                >
-                  <option value="">Select School</option>
-                  {schools.map((s) => (
-                    <option key={s.id} value={s.id}>
-                      {`${s.name} (${s.code.toUpperCase()})`}
-                    </option>
-                  ))}
-                </select>
+        </div>
+        <div className="flex-1 px-5 lg:px-0 lg:flex-1/2 flex justify-center items-center">
+          <div className="w-full max-w-md bg-(--bg-card) rounded-2xl shadow-none border border-(--border) p-6 sm:p-8">
+            <div className="mb-5 text-center">
+              <div className="mx-auto mb-4 flex h-12 w-12 items-center justify-center rounded-full border border-(--primary)/30">
+                <Shield className="h-6 w-6 text-(--primary)" />
               </div>
+              <h1 className="text-xl font-bold uppercase tracking-wide">Portal Login</h1>
+              <p className="text-sm text-(--text-muted) font-medium">
+                Secure access for schools & staff
+              </p>
             </div>
-
-            {/* Email */}
-            <div>
-              <label className="text-sm text-[var(--text-muted)]">School ID / Username</label>
-              <div className="relative">
-                <Mail className="absolute left-3 top-3 h-4 w-4 text-[var(--text-muted)]" />
-                <input
-                  type="text"
-                  required
-                  placeholder="enter school id"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className="w-full pl-10 pr-3 py-2 rounded-lg bg-transparent border border-[var(--border)]"
-                />
+            <form onSubmit={handleLogin} className="space-y-4">
+              <div>
+                <label className="text-sm font-medium text-(--text-muted)">School</label>
+                <div className="relative">
+                  <School className="absolute left-3.5 top-3.5 h-4 w-4 text-(--text-muted)" />
+                  <select
+                    required
+                    value={schoolId}
+                    onChange={(e) => {
+                      setSchoolId(e.target.value);
+                      setSchoolCode(e.target.value != '' ? e.target.options[e.target.selectedIndex].text.split(' (')[1].replace(')', '') : '')
+                    }}
+                    className="w-full pl-10 pr-3 py-2 rounded-lg border border-(--border) focus:outline-none focus:ring-2 focus:ring-[var(--primary)]"
+                  >
+                    <option value="">Select School</option>
+                    {schools.map((s) => (
+                      <option key={s.id} value={s.id}>
+                        {`${s.name} (${s.code.toUpperCase()})`}
+                      </option>
+                    ))}
+                  </select>
+                </div>
               </div>
-            </div>
-
-            {/* Password */}
-            <div>
-              <label className="text-sm text-[var(--text-muted)]">Password</label>
-              <div className="relative">
-                <Lock className="absolute left-3 top-3 h-4 w-4 text-[var(--text-muted)]" />
-                <input
-                  type="password"
-                  required
-                  placeholder="enter password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  className="w-full pl-10 pr-3 py-2 rounded-lg bg-transparent border border-[var(--border)]"
-                />
+              <div>
+                <label className="text-sm font-medium text-[var(--text-muted)]">School ID / Username</label>
+                <div className="relative">
+                  <Mail className="absolute left-3.5 top-3.5 h-4 w-4 text-[var(--text-muted)]" />
+                  <input
+                    type="text"
+                    required
+                    placeholder="enter school id"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="w-full pl-10 pr-3 py-2 rounded-lg bg-transparent border border-[var(--border)]"
+                  />
+                </div>
               </div>
-            </div>
+              <div>
+                <label className="text-sm font-medium text-[var(--text-muted)]">Password</label>
+                <div className="relative">
+                  <Lock className="absolute left-3.5 top-3.5 h-4 w-4 text-[var(--text-muted)]" />
+                  <input
+                    type="password"
+                    required
+                    placeholder="enter password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="w-full pl-10 pr-3 py-2 rounded-lg bg-transparent border border-[var(--border)]"
+                  />
+                </div>
+              </div>
 
-            {error && (
-              <p className="text-sm text-[var(--danger)]">{error}</p>
-            )}
+              {error && (
+                <p className="text-sm text-[var(--danger)]">{error}</p>
+              )}
 
-            <button
-              type="submit"
-              className="w-full mt-2 flex items-center justify-center gap-2 bg-[var(--primary)] hover:bg-[var(--primary-hover)] text-white py-2 rounded-lg font-medium transition"
-            >
-              <LogIn size={18} />
-              Login
-            </button>
-          </form>
+              <button
+                type="submit"
+                className="w-full h-11 mt-2 flex items-center justify-center gap-2 bg-[var(--primary)] hover:bg-[var(--primary-hover)] text-white py-2 rounded-lg font-medium transition"
+              >
+                Sign in
+                <ChevronRight className="h-4 w-4 stroke-3" />
+              </button>
+              <div className="text-center text-(--text-muted) -mt-1 font-medium text-xs"><p>Need access? Contact School Admin</p></div>
+            </form>
+          </div>
         </div>
       </div>
     </>
